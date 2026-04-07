74823ddda492fa2a8e60396f60d56cb1dcd67049
matrixguy69 <blouchsaab587@gmail.com>
2026-04-07 16:43:47 +0500

    Create effects.js

---
+++ effects.js
@@ -0,0 +1,10 @@
+particlesJS("particles", {
+  particles:{
+    number:{value:60},
+    color:{value:"#00ff88"},
+    shape:"circle",
+    opacity:{value:0.5},
+    size:{value:3},
+    move:{enable:true,speed:1}
+  }
+});
